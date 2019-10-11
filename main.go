44f5d2801c9bbba648eca7fd15ed659b3ed839b3
Trey Decker <treydecker75@gmail.com>
2019-10-11 17:48:39 -0600

    inflight work, docker images not working. Need more work

--- main.go
+++ main.go
@@ -95,7 +95,7 @@ func handleRequests() {
 }
 
 func main() {
-	db, err = gorm.Open("postgres", "host=127.0.0.1 port=5432 user=redeamapi dbname=book_store password=N+JmM7za4^zvq4ezK-dcc*dbszRWQ*9fDc$W9Ud sslmode=disable")
+	db, err = gorm.Open("postgres", "host=db port=5432 user=redeamapi dbname=book_store password=N+JmM7za4^zvq4ezK-dcc*dbszRWQ*9fDc$W9Ud sslmode=disable")
 	if err != nil {
 		log.Fatalf("Error opening postgres instance: %s", err)
 	}
