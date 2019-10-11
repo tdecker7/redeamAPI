328099a3f140b1209ee559eec1754c4e920a0d0a
Trey Decker <treydecker75@gmail.com>
2019-10-11 14:39:27 -0600

    handling base route

--- main.go
+++ main.go
@@ -1,16 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
+
+type Book struct {
+	Title       string    `json:"title"`
+	Author      string    `json:"author"`
+	Publisher   string    `json:"publisher"`
+	PublishDate time.Time `json:"publish_date"`
+}
+
+func handleBaseRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to my bookstore")
+	fmt.Println("Hit the base route")
+}
+
+func handleRequests() {
+	log.Println("Starting development server 127.0.0.1:9000")
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", handleBaseRoute)
+	log.Fatal(http.ListenAndServe(":9000", myRouter))
+}
 
 func main() {
 	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=redeamapi dbname=book_store password=N+JmM7za4^zvq4ezK-dcc*dbszRWQ*9fDc$W9Ud sslmode=disable")
 	if err != nil {
 		log.Fatalf("Error opening postgres instance: %s", err)
 	}
+	log.Println("Connection to database established successfully.")
 	defer db.Close()
+
+	db.AutoMigrate(&Book{})
+	handleRequests()
 }
